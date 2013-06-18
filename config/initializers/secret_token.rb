5889de0dae305dc1e3fae8ef85ac39c2439060f3
Tomas Ruta <tomas.ruta@gmail.com>
2013-06-19 01:05:48 +0200

    Post-pin effup commit of all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '3495ab8f617f8b7591bd7c52f322e25876c2a41c84cb1c062f1791d1f6ee7566a4d0269b25b956691490ce21c0bda2219ff01ab06c394cb3abe76f9028743e25'
